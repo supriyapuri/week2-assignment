4c0d56be823c4cf0114afc670d2690beea8b2eb2
supriyapuri <supriyapuri11@gmail.com>
2020-04-20 09:36:56 -0700

    add blog-homepage

---
+++ blog-homepage/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Screen from './screen.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Screen />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ blog-homepage/src/screen.js
@@ -0,0 +1,179 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import foryouSection from './your-articles.json'
+import missedArticle from './missed-articles.json'
+
+class Screen extends React.Component {
+
+    constructor(props) {
+        super(props);
+         this.state= {
+             forYouSection: foryouSection,
+             missedArticle: missedArticle
+        }
+    }
+
+    renderForYouSection() {
+        return <ForYouSection forYouSection={this.state.forYouSection} />;
+    }
+
+    renderMissedArticleSection() {
+    return <MissedArticleSection missedArticle={this.state.missedArticle}/>;
+    }
+
+    render(){
+        return(
+        <div> 
+            {this.renderForYouSection()}
+ 
+            {this.renderMissedArticleSection()}
+
+        </div>
+
+
+        );
+    }
+        
+
+}
+
+class ForYouSection extends React.Component {
+    render() {
+        const {forYouSection} = this.props;
+        return (
+            <div>
+                <h2>For you</h2>
+                <hr />
+                    <div className= "cards_foryou">
+                        {forYouSection.map((card)=> 
+                   
+                        <ForYouCard card={card} /> 
+                  
+                        )}
+                  </div>
+                
+            </div>
+        ) ;
+
+    }
+
+}
+
+class MissedArticleSection extends React.Component {
+    render() {
+        const {missedArticle} = this.props;
+        return (
+            <div>
+                <h2>In case you missed it</h2>
+                <hr />
+                    <div className= "cards_missed">
+                        {missedArticle.map((card)=> 
+                   
+                        <MissedArticleCard card={card} /> 
+                  
+                        )}
+                  </div>
+                
+            </div>
+        ) ;
+
+    }
+
+}
+
+
+class ForYouCard extends React.Component {
+    render() {
+
+        const hasAudioAvailable = this.props.card.hasAudioAvailable;
+        const memberPreview = this.props.card.memberPreview;
+        const isMediumMember= this.props.card.author.isMediumMember;
+        return(
+            
+                <div className= "card">
+                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet"></link>
+                    <img src={this.props.card.image} alt=" " className= "card_image" />
+                    <div className= "card_content">
+                        <div className="content_top">
+                            <div className= "top_condition">
+                                {hasAudioAvailable ? (<i className="material-icons">volume_up</i>) : null}
+                                {hasAudioAvailable ? (<p className="audio">Audio Available</p>) : null}
+                                {memberPreview ? (<i className="material-icons">star_outline</i>) : null}
+                            </div>
+                            <h3>{this.props.card.title}</h3>
+                            <p className= "description">{this.props.card.description}</p>
+                        </div>
+                        <div className= "content_bottom">
+                            <div className = "author_image">
+                                {isMediumMember? (<img src={this.props.card.author.image} alt=" " className= "author_image2" />) 
+                                : (<img src={this.props.card.author.image} alt=" " className= "author_image1" />)}
+
+                            </div>
+                            
+                            <div className = "bottom_middle">
+                                <p className= "author_name">{this.props.card.author.name}</p>
+                                <p className= "doc_details">{this.props.card.postedDate} . {this.props.card.minutesToRead} min read </p>
+                            </div>
+                            
+                            <i className="material-icons">bookmark_border</i>
+                            
+                        </div>
+                    </div>
+
+            </div>
+                
+
+
+        );
+    }
+}
+
+class MissedArticleCard extends React.Component{
+    render() {
+        const hasAudioAvailable = this.props.card.hasAudioAvailable;
+        const memberPreview = this.props.card.memberPreview;
+        const isMediumMember= this.props.card.author.isMediumMember;
+        return(
+            <div className= "card2">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet"></link>
+            <img src={this.props.card.image} alt=" " className= "card_image2" />
+            <div>
+                <div>
+                    <div className= "top_condition">
+                        {hasAudioAvailable ? (<i className="material-icons">volume_up</i>) : null}
+                        {hasAudioAvailable ? (<p className="audio">Audio Available</p>) : null}
+                        {memberPreview ? (<i className="material-icons">star_outline</i>) : null}
+                    </div>
+                    <div className = "title">
+                        <h3>{this.props.card.title}</h3>
+                        <p className= "description2">{this.props.card.description}</p>
+                    </div>
+                    
+                </div>
+                <div className= "content_bottom">
+                    <div className = "author_image">
+                         {isMediumMember? (<img src={this.props.card.author.image} alt=" " className= "author_image2" />) 
+                                : (<img src={this.props.card.author.image} alt=" " className= "author_image1" />)}
+
+                    </div>
+
+                    
+                    <div className = "bottom_middle">
+                        <p className= "author_name">{this.props.card.author.name}</p>
+                        <p className= "doc_details">{this.props.card.postedDate} . {this.props.card.minutesToRead} min read </p>
+                    </div>
+                    
+                    <i className="material-icons">bookmark_border</i>
+                    
+                </div>
+            </div>
+ 
+    </div>
+
+        );
+
+    }
+
+}
+
+export {Screen as default};
